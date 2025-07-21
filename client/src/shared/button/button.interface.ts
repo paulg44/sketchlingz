3275b60358a3595ebba7f35ad7ef122046352ff3
Paul Garton <paulgarton1984@hotmail.com>
2025-07-21 22:01:44 +0100

    test ant design button

---
+++ client/src/shared/button/button.interface.ts
@@ -0,0 +1,4 @@
+export interface ISharedButton {
+  type?: "button" | "submit" | "reset";
+  appearance?: "primary" | "default" | "dashed" | "text" | "link";
+}
---
+++ client/src/shared/button/button.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import type { ISharedButton } from "./button.interface";
+
+const SharedButton = ({ type, appearance }: ISharedButton) => {
+  return (
+    <Button type={appearance} htmlType={type}>
+      Test Button
+    </Button>
+  );
+};
+
+export default SharedButton;

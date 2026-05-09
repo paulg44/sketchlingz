1a805e7f8779b2f12bc3358bf84f11106191cd0d
Paul Garton <paulgarton1984@hotmail.com>
2026-05-09 23:30:02 +0100

    just show random item on game page

--- portal/src/shared/category/category-select.tsx
+++ portal/src/shared/category/category-select.tsx
@@ -1,13 +1,10 @@
 import { useCategory } from '../../core/providers/category-context';
-import { useItem } from '../../core/providers/item-context';
 import SharedButton from '../button/button';
 import SharedCard from '../card/card';
-import RandomItemDisplay from '../random-item-display/random-item-display';
 
 const CategorySelect = () => {
   const { categories, selectedCategory, selectCategory } = useCategory();
-  const categorySelected = selectedCategory ? `Selected Category: ${selectedCategory.name}` : 'No category selected';
-  const { randomItem, respinItem } = useItem();
+
   return (
     <>
       {categories.map((category, index) => (
@@ -20,9 +17,7 @@ const CategorySelect = () => {
           />
         </SharedCard>
       ))}
-      <h2>{categorySelected}</h2>
-      {randomItem && <RandomItemDisplay {...randomItem} imageUrl={randomItem.imageUrl ?? ''} />}
-      <SharedButton labelKey='Respin Item' onClick={respinItem} appearance='primary' />
+      {selectedCategory && <h2>Selected: {selectedCategory.name}</h2>}
     </>
   );
 };

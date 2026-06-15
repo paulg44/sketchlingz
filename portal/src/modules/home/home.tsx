8921819052a442106c8096e2e5d99b90a045075e
Paul Garton <paulgarton1984@hotmail.com>
2026-06-15 21:09:58 +0100

    add logos and images

--- portal/src/modules/home/home.tsx
+++ portal/src/modules/home/home.tsx
@@ -1,7 +1,14 @@
+import headerImage from '../../assets/images/sketchlingz_header.png.png';
+import SharedButton from '../../shared/button/button';
+
 const Homepage = () => {
   return (
-    <div>
-      <h1>Sketchlingz</h1>
+    <div className='flex flex-col items-center'>
+      <img src={headerImage} alt='main header image' className='border' />
+      <div>
+        <SharedButton labelKey='Play Games' />
+        <SharedButton labelKey='Just Draw' />
+      </div>
     </div>
   );
 };
--- portal/src/shared/navbar/navbar.tsx
+++ portal/src/shared/navbar/navbar.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import monsterLogo from '../../assets/images/sketchlingz_monster.png.png';
 
 const Navbar = () => {
   return (
     <nav className='border-b-amber-600 display flex items-center justify-between border-b-2 bg-gray-800 p-4 text-white'>
-      <img src='/logo.png' alt='Logo' className='h-8 w-8' />
+      <img src={monsterLogo} alt='Logo' className='h-10' />
 
       <div>
         <button className=''>Sign In</button>

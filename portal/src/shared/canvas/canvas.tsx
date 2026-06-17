e594c0b91f0ff23fd4e4e9ba2de012fbc90c3c30
Paul Garton <paulgarton1984@hotmail.com>
2026-06-17 19:21:18 +0100

    change navbar to sidebar

--- portal/src/shared/canvas/canvas.tsx
+++ portal/src/shared/canvas/canvas.tsx
@@ -3,7 +3,7 @@ import CanvasToolbar from './canvas-toolbar';
 
 const Canvas = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full h-full relative'>
+    <div className='flex flex-col items-center justify-center w-full h-auto relative'>
       <div className='w-full absolute bottom-0 flex justify-end z-10'>
         <CanvasToolbar />
       </div>
--- portal/src/shared/navbar/navbar.tsx
+++ portal/src/shared/navbar/navbar.tsx
@@ -3,13 +3,13 @@ import monsterLogo from '../../assets/images/sketchlingz_monster.png.png';
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between p-4 text-white bg-blue-800'>
-      <img src={monsterLogo} alt='Logo' className='h-10' />
-
-      <div>
-        <button className=''>Sign In</button>
-
-        <ul className='flex space-x-4'>
+    <nav className='fixed top-0 left-0 z-40 w-54 h-full p-4 text-white bg-blue-800 border-r border-gray-300'>
+      <div className='h-full flex flex-col items-start justify-between p-4'>
+        <div>
+          <img src={monsterLogo} alt='Logo' />
+          <button className=''>Sign In</button>
+        </div>
+        <ul className='flex flex-col gap-2'>
           <Link to='/'>Home</Link>
           <Link to='settings'>Settings</Link>
         </ul>
